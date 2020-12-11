00c38a177fa86de8d7c072073748ff509133e548
emadam <tech@weinwork.com>
2020-12-11 22:21:13 +0900

    Add accessor in province
    Various data accessors added

--- src/production/Province.js
+++ src/production/Province.js
@@ -25,4 +25,36 @@ class Province{
         this._producers.push(arg);
         this._totalProduction += arg.production;
     }
+
+    get name() {
+        return this._name;
+    }
+
+    get producers(){
+        return this._producers.slice();
+    }
+
+    get totalProduction(){
+        return this._totalProduction;
+    }
+
+    set totalProduction(arg){
+        this._totalProduction = arg;
+    }
+
+    get demand() {
+        return this._demand;
+    }
+
+    set demand(arg) {
+        this._demand = parseInt(arg);
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(arg) {
+        this._price = parseInt(arg);
+    }
 }
