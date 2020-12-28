2182483206ab983b7ee913e5ce2b7acbf777ba98
emadam <tech@weinwork.com>
2020-12-28 20:50:44 +0900

    Remove unused function
    Remove unused printOwing function and Clock variable

--- test/refactoring/index.spec.js
+++ test/refactoring/index.spec.js
@@ -1,11 +1,6 @@
 const Owing = require('../../src/refactoring/Owing');
 const should = require('should');
 
-const Clock = {
-    today: new Date('2020-12-28 08:19:00')
-};
-
-
 const invoice = {
     orders: [
         {amount: 3000},
@@ -15,25 +10,6 @@ const invoice = {
     ],
     dueDate: new Date(),
     customer: '이동열'
-}
-
-function printOwing(aInvoice) {
-    let outstanding = 0;
-
-    console.log("******************");
-    console.log("***** 고객 채무 *****");
-    console.log("******************");
-
-    for (const o of aInvoice.orders) {
-        outstanding += o.amount;
-    }
-
-    const today = Clock.today;
-    aInvoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()+30);
-
-    console.log(`고객명: ${aInvoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${aInvoice.dueDate.toLocaleDateString()}`);
 }
 
 describe('Owing', function (){
