367cfeeb9a9db1899e835c23ab1646bb1aefbe93
emadam <tech@weinwork.com>
2020-12-30 22:32:43 +0900

    Update price function
    Create a variable containing the basic price of the product

--- test/refactoring/index.spec.js
+++ test/refactoring/index.spec.js
@@ -13,7 +13,8 @@ const invoice = {
 }
 
 function price(order){
-    return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    const basePrice = order.quantity * order.itemPrice;
+    return basePrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + Math.min(basePrice * 0.1, 100);
 }
 
 describe('Owing', function (){
